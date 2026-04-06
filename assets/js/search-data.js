a5ffb4b953e5016b50af9bb3628ca1be28091145
skwn-j <minimus@snu.ac.kr>
2026-04-06 02:08:14 +0000

    Deploying to gh-pages from @ skwn-j/skwn-j.github.io@95a670a59baf122937d1a9b356baad7698121f6e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-started-my-collaborative-research-in-cmu-dig",
+          title: 'I started my collaborative research in CMU DIG!',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-will-be-participating-chi-2026-barcelona-and-also-pacificvis2026-sydney",
+          title: 'I will be participating CHI 2026@Barcelona and also PacificVis2026@Sydney!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
